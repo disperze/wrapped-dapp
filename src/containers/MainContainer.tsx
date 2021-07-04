5efefd5a58515bd817085a828d390c6abed25008
Giancarlos Salas <giansalex@gmail.com>
2021-07-04 13:02:27 -0500

    Update design

--- src/containers/MainContainer.tsx
+++ src/containers/MainContainer.tsx
@@ -64,80 +64,54 @@ class MainContainer extends Component<IProps, IState> {
     const tabItems = (
       <TabItems value={active} >
         <TabItem>
-            <Card dark inset className="pa-5">
-                <CardContent>
-                <div className="row">
-                    <div className="col-md-6">
-                        <Card
+        <Card dark inset style={{ padding: '16px' }}>
+                    <Caption disabled secondary className="mb-3">
+                        Available: 200 JUNO
+                    </Caption>
+                    <TextField
+                        label="Enter amount"
+                        style={{ margin: "0" }}
+                    ></TextField>
+                    <div className="text-center">
+                        <IconButton
+                            text={false}
                             dark
-                            elevation={3}
-                            style={{
-                                display: 'flex',
-                                width: '194px',
-                                height: '194px',
-                                borderRadius: '150px',
-                                alignItems: 'center',
-                                justifyContent: 'center'
-                            }}
-                            className="ma-auto"
+                            size='small'
+                            rounded
                         >
-                            <Card flat>
-                                <Icon path={mdiRun} size={2.5} color='var(--primary)' />
-                                <H5 style={{ padding: '4px 0px' }}>8,690</H5>
-
-                                <Caption secondary className="text-center">
-                                    WJUNO
-                                </Caption>
-                            </Card>
-                        </Card>
+                            <Icon
+                                path={mdiChevronRight}
+                                size={0.7}
+                                color='var(--primary)'
+                            />
+                        </IconButton>
                     </div>
-                    <div className="col-md-6">
-                    <Card dark rounded={false} elevation={2} style={{ padding: '16px', marginTop: "30px" }}>
-                                <Caption disabled secondary className="mb-3" style={{marginLeft: "12px"}}>
-                                    Available: 200 JUNO 
-                                </Caption>
-                        <TextField
-                            label="Enter amount"
-                        ></TextField>
-                                        <div className="text-right">
-                                        <IconButton
-                                            text={false}
-                                            dark
-                                            size='small'
-                                            rounded
-                                        >
-                                         <Icon
-                                                path={mdiChevronRight}
-                                                size={0.7}
-                                                color='var(--primary)'
-                                            />
-                                         
-                                        </IconButton> 
-                                        </div>   
-                    </Card>
-                    </div>
-                </div>
-                </CardContent>
-            </Card>
+                </Card>
         </TabItem>
         <TabItem>
-        <Card dark inset>
-            <CardContent>
-                <div>
-                    Balance: 100 <code>WJUNO</code>
-                </div>
-                <TextField
-                    append='WJUNO'
-                    label='Enter amount'
-                    className='my-3'
-                ></TextField>
-            </CardContent>
-            <CardAction>
-                <Button dark depressed>
-                    Withdraw
-                </Button>
-            </CardAction>
-        </Card>
+        <Card dark inset style={{ padding: '16px' }}>
+                    <Caption disabled secondary className="mb-3">
+                        Available: 200 WJUNO
+                    </Caption>
+                    <TextField
+                        label="Enter amount"
+                        style={{ margin: "0" }}
+                    ></TextField>
+                    <div className="text-center">
+                        <IconButton
+                            text={false}
+                            dark
+                            size='small'
+                            rounded
+                        >
+                            <Icon
+                                path={mdiChevronRight}
+                                size={0.7}
+                                color='var(--primary)'
+                            />
+                        </IconButton>
+                    </div>
+                </Card>
         </TabItem>
       </TabItems>
     );
@@ -151,17 +125,49 @@ class MainContainer extends Component<IProps, IState> {
                     &nbsp;<span style={{ fontSize: '24px' }}>&#9729;</span>&nbsp;Connect wallet&nbsp;
                 </Fab>
             </div>
-            <div className="row">
+            <div className="row" style={{margin: "80px 0"}}>
                 <div className="col-md-6 ma-auto">
-                    <Card dark className="ma-3 pa-5">
-                        <Tabs
-                        value={active}
-                        onChange={({ active }: { active: any}) => this.setState({ active })}
-                        >
-                        <Tab>Deposit</Tab>
-                        <Tab>Withdraw</Tab>
-                        </Tabs>
-                        {tabItems}
+                    <Card dark className="pa-5">
+                    <CardContent>
+                        <div className="row">
+                            <div className="col-md-6" style={{display: "flex"}}>
+                                <Card
+                                    dark
+                                    elevation={3}
+                                    style={{
+                                        display: 'flex',
+                                        width: '194px',
+                                        height: '194px',
+                                        borderRadius: '150px',
+                                        alignItems: 'center',
+                                        justifyContent: 'center'
+                                    }}
+                                    className="ma-auto"
+                                >
+                                    <Card flat>
+                                        <Icon path={mdiRun} size={2.5} color='var(--primary)' />
+                                        <H5 style={{ padding: '4px 0px' }}>8,690</H5>
+
+                                        <Caption secondary className="text-center">
+                                            WJUNO
+                                        </Caption>
+                                    </Card>
+                                </Card>
+                            </div>
+                            <div className="col-md-6">
+                                <Card dark className="pa-5">
+                                    <Tabs
+                                        value={active}
+                                        onChange={({ active }: { active: any }) => this.setState({ active })}
+                                    >
+                                        <Tab>Deposit</Tab>
+                                        <Tab>Withdraw</Tab>
+                                    </Tabs>
+                                    {tabItems}
+                                </Card>
+                            </div>
+                        </div>                        
+                        </CardContent>
                     </Card>
                 </div>
             </div>
